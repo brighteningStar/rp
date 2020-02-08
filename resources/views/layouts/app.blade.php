e6f0288da5d3c7686a0728d0957aa78028e7f997
Ahsan Ali <ahsan@softpyramid.com>
2020-02-08 19:57:16 +0500

    fixed the bootstrap for vue js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,21 +19,27 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-logo">
-        <a href="#"><b>Admin</b>LTE</a>
+<div id="app">
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="#"><b>Admin</b>LTE</a>
+        </div>
+        <!-- /.login-logo -->
+        @yield('content')
     </div>
-    <!-- /.login-logo -->
-    @yield('content')
 </div>
+
 <!-- /.login-box -->
 
 <!-- jQuery -->
+
 <script src="../../plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
 <!-- AdminLTE App -->
 <script src="../../dist/js/adminlte.min.js"></script>
+
 
 </body>
 </html>
