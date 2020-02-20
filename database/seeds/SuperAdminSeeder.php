abe7e1019c015c5b57aa07e3c064f82693405e86
Ahsan Ali <ahsan@softpyramid.com>
2020-02-21 00:02:36 +0500

    added super admin seeder

---
+++ database/seeds/SuperAdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $super_admin = \App\Models\User::firstOrCreate([
+                'email' => 'system@mailinator.com',
+            ],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('testing1234'),
+
+            ]);
+        $super_admin->assignRole('super_admin');
+    }
+}
